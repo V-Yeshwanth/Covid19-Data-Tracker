199e44337690d387fe2b650ee6b2b63dc07f7a73
yeshi <yeshi@yeshi.com>
2021-05-31 16:59:37 +0530

    Covid19 Data Tracker

---
+++ app.js
@@ -0,0 +1,65 @@
+var tableData = document.getElementById('data');
+var sectionData = document.getElementById('section-data');
+
+function fetchData(){
+    var section = document.getElementById('section');
+    section.classList.add('section-height');
+
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function(){
+        if(this.readyState === 4){
+            if(this.status === 200){
+                var response = JSON.parse(this.response);
+                console.log(response);
+                console.log(response.Countries);
+
+                var col = [];
+                for (var i = 0; i < 10; i++) {
+                    for (var key in response.Countries[i]) {
+                        if (col.indexOf(key) === -1) {
+                            col.push(key);
+                        }
+                    }
+                }
+                console.log(col[1]);
+                var table = document.createElement("table");
+                table.classList.add('table');
+                table.classList.add('table-responsive');
+                table.classList.add('hover-table');
+                var tr = table.insertRow(-1);
+                for (var i = 0; i < col.length; i++) {
+                    var th = document.createElement("th");     
+                    if(col[i] === "Country" || col[i] === "NewConfirmed"
+                    || col[i] === "NewDeaths" || col[i] === "NewRecovered"){    
+                        th.innerHTML = col[i];
+                        tr.appendChild(th);
+                    }
+                }
+                for (var i = 0; i < response.Countries.length; i++) {
+                    tr = table.insertRow(-1);
+                    for (var j = 0; j < col.length; j++) {
+                        if(col[j] === "Country" || col[j] === "NewConfirmed"
+                        || col[j] === "NewDeaths" || col[j] === "NewRecovered"){
+                            var tabCell = tr.insertCell(-1);
+                            tabCell.innerHTML = response.Countries[i][col[j]];
+                        }
+                    }
+                }
+                var divContainer = document.getElementById("show-table-data");
+                divContainer.innerHTML = " ";
+                divContainer.appendChild(table);
+
+                tableData.innerHTML = `
+                        <td>${response.Global.NewConfirmed}</td>
+                        <td>${response.Global.NewDeaths}</td>
+                        <td>${response.Global.NewRecovered}</td>
+                    `;
+
+            } else {
+                console.log("Call Failed");
+            }
+        }
+    }
+    http.open('GET', 'https://api.covid19api.com/summary',true);
+    http.send();
+}
